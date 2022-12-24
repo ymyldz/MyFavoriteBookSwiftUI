dee47ba1f11ef846d6a02365020e6da42f402e3f
ymyldz <113609896+ymyldz@users.noreply.github.com>
2022-12-25 00:13:56 +0300

    Initial Commit

---
+++ MyFavoriteBookSwiftUI/MyFavoriteBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavoriteBookSwiftUIApp.swift
+//  MyFavoriteBookSwiftUI
+//
+//  Created by Yusuf Mert Yıldız on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFavoriteBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
